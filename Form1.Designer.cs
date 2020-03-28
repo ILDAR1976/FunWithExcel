05b7bd29aeb0f638539b8921ce4c5ad54d2417eb
Ильдар Ахметов <akhmetov.ih@yandex.ru>
2020-03-28 14:39:27 +0300

    first code

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FunWithExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load_sheet_excel = new System.Windows.Forms.Button();
+            this.info = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // load_sheet_excel
+            // 
+            this.load_sheet_excel.Location = new System.Drawing.Point(12, 13);
+            this.load_sheet_excel.Name = "load_sheet_excel";
+            this.load_sheet_excel.Size = new System.Drawing.Size(160, 23);
+            this.load_sheet_excel.TabIndex = 0;
+            this.load_sheet_excel.Text = "Load";
+            this.load_sheet_excel.UseVisualStyleBackColor = true;
+            this.load_sheet_excel.Click += new System.EventHandler(this.load_sheet_excel_Click);
+            // 
+            // info
+            // 
+            this.info.Location = new System.Drawing.Point(13, 60);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(558, 275);
+            this.info.TabIndex = 1;
+            this.info.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 347);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.load_sheet_excel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load_sheet_excel;
+        private System.Windows.Forms.RichTextBox info;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunWithExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void load_sheet_excel_Click(object sender, EventArgs e)
+        {
+            String filePath = @"V:\projects\cs\example\10_result.xls";
+            String connectionString = String.Empty;
+            
+            info.Clear();
+
+            try
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU", true);
+                Thread.CurrentThread.CurrentCulture.ClearCachedData();
+
+                if (filePath.Substring(filePath.LastIndexOf('.')).ToLower() == ".xlsx")
+                    connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=\"Excel 12.0;HDR=" + "No" + ";IMEX=0\""; 
+                else
+                    connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=\"Excel 8.0;HDR=" + "No" + ";IMEX=0\"";
+
+                OleDbConnection con = new System.Data.OleDb.OleDbConnection(connectionString);
+                con.Open(); 
+                DataTable schemaTable = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                DataRow schemaRow = schemaTable.Rows[0]; 
+                string sheet = schemaRow["TABLE_NAME"].ToString(); 
+                
+                if (!sheet.EndsWith("_")) { string query = "SELECT * FROM [" + sheet + "]"; 
+                    OleDbDataAdapter daexcel = new OleDbDataAdapter(query, con);
+                    schemaTable.Locale = CultureInfo.CurrentCulture;
+                    daexcel.Fill(schemaTable);
+                       
+                    DataSet ds = new DataSet();
+                    ds.Tables.Add(schemaTable);
+                    int c = 0;
+                    foreach (DataTable dt in ds.Tables)
+                    {
+                        
+                        // перебор всех строк таблицы
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            // получаем все ячейки строки
+                            var cells = row.ItemArray;
+                            if ((++c) != 1) { 
+                                foreach (object cell in cells) info.AppendText(cell.ToString().Trim() + " ");
+                                info.AppendText("\n");
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                con.Close();
+                con.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
